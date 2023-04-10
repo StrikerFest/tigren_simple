19182c224667f306816213df70430fea2b1fe2e5
The Anh <trinhtheanh789@Gmail.com>
2023-04-10 16:29:37 +0700

    Challenge 2 - Pass config data to your JS file via the template, then by layout using jsLayout

--- app/design/frontend/Tigren/simple/Magento_Theme/web/js/hello.js
+++ app/design/frontend/Tigren/simple/Magento_Theme/web/js/hello.js
@@ -5,6 +5,7 @@ function($) {
   return (config) => {
   $(document).ready(function($){
           console.log("Hello from magento theme, this take title from phtml:",config.title);
+          console.log("Hello from magento theme, this take desc from phtml:",config.desc);
       });
   }
 
